8f5ab203f6d64d32fe1a1d2bf4b81fd2ace56875
josephfaix <113110962+josephfaix@users.noreply.github.com>
2022-09-09 08:18:08 -0400

    Assignment 1 9/9
    Completed programs "Debug.py" and "Hello World"

--- Assignment 1/Debug.py
+++ Assignment 1/Debug.py
@@ -2,7 +2,7 @@
 #Assignment 1: Debug.py
 #Fix any syntax errors you can spot and fulfill any other requests
 def main():
-    print(hello)
+    print("hello")
     print(3)
     print(10 + 10)
 
@@ -15,15 +15,15 @@ def main():
     print("2 + 2")
 
     #insert your name into this variable
-    name = ""
+    name = "Joey"
     print("Hello, my name is ", name)
 
     #insert your best friend's name here and have it print out
-    name2 = ""
-    print("Hello, my best friend's name is ", name)
+    name2 = "Casey"
+    print("Hello, my best friend's name is ", name2)
     
     #put your age here
-    age = 
+    age = 18
     print("I'm " + name + " and i'm " + str(age) + " years old")
     
  
